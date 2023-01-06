0c53cd842bdb78cefa8d2e738a994cf6e71ebbb8
Adrian <adrian.schneider.pr@gmail.com>
2023-01-06 23:10:40 +0100

    semi working gui

--- app/gui/mainwindow.cpp
+++ app/gui/mainwindow.cpp
@@ -9,7 +9,24 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
 
-    connect(ui->btn4, &QPushButton::released, this, [this]{ handleButtonPress(4); });
+    connect(ui->btn0, &QPushButton::released, this, [this]{ numericPressed(0); });
+    connect(ui->btn1, &QPushButton::released, this, [this]{ numericPressed(1); });
+    connect(ui->btn2, &QPushButton::released, this, [this]{ numericPressed(2); });
+    connect(ui->btn3, &QPushButton::released, this, [this]{ numericPressed(3); });
+    connect(ui->btn4, &QPushButton::released, this, [this]{ numericPressed(4); });
+    connect(ui->btn5, &QPushButton::released, this, [this]{ numericPressed(5); });
+    connect(ui->btn6, &QPushButton::released, this, [this]{ numericPressed(6); });
+    connect(ui->btn7, &QPushButton::released, this, [this]{ numericPressed(7); });
+    connect(ui->btn8, &QPushButton::released, this, [this]{ numericPressed(8); });
+    connect(ui->btn9, &QPushButton::released, this, [this]{ numericPressed(9); });
+
+    connect(ui->btnDel, &QPushButton::released, this, [this]{ deletePressed(); });
+    connect(ui->btnGo, &QPushButton::released, this, [this]{ goPressed(); });
+
+    m_QuestionFactory = std::shared_ptr<SumFactory>(new SumFactory({0, 9}, 2));
+    m_Play = std::shared_ptr<Play>(new Play(3, m_QuestionFactory));
+
+    showNextQuestion();
 }
 
 MainWindow::~MainWindow()
@@ -17,8 +34,36 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-void MainWindow::handleButtonPress(int n)
+void MainWindow::numericPressed(int n)
 {
     std::cout << "Button pressed " << n << std::endl;
+
+    QString currentText = ui->resEdit->text();
+    currentText.append(QString::number(n));
+    ui->resEdit->setText(currentText);
+
+    std::cout << "Current number: " << currentText.toStdString() << std::endl;
+}
+
+void MainWindow::goPressed()
+{
+    std::cout << "Go pressed" << std::endl;
+    showNextQuestion();
+}
+
+void MainWindow::deletePressed()
+{
+    std::cout << "Delete pressed" << std::endl;
+    ui->resEdit->setText("");
+}
+
+void MainWindow::showNextQuestion()
+{
+    std::shared_ptr<Question> q = m_Play->nextQuestion();
+    if( q.get() != nullptr )
+    {
+        QString qText = QString::fromStdString(q->toString()) + " = ";
+        ui->qLabel->setText(qText);
+    }
 }
 
--- app/gui/mainwindow.h
+++ app/gui/mainwindow.h
@@ -3,9 +3,37 @@
 
 #include <QMainWindow>
 
+#include "play.h"
+#include "question.h"
+
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
+
+
+// only for fast testing
+class SumFactory : public QuestionFactory
+{
+public:
+    SumFactory(std::pair<int,int> numberRange, size_t length): QuestionFactory()
+    {
+        m_numberRange = numberRange;
+        m_length = length;
+    }
+
+    ~SumFactory()
+    {
+    }
+
+    std::shared_ptr<Question> createQuestion() override
+    {
+        return std::shared_ptr<Question>(new SumQuestion(m_numberRange, m_length) );
+    }
+
+    std::pair<int,int> m_numberRange;
+    size_t m_length;
+};
+
 
 class MainWindow : public QMainWindow
 {
@@ -15,10 +43,17 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private:
+    void showNextQuestion();
+
 private slots:
-  void handleButtonPress(int n);
+  void numericPressed(int n);
+  void goPressed();
+  void deletePressed();
 
 private:
     Ui::MainWindow *ui;
+    std::shared_ptr<QuestionFactory> m_QuestionFactory;
+    std::shared_ptr<Play> m_Play;
 };
 #endif // MAINWINDOW_H
